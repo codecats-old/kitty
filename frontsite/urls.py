345fdd5984e4af89ad73bd688a68b696aa00926b
ssstrz <codecats@gmail.com>
2014-05-24 22:13:26 +0200

    index started

--- frontsite/urls.py
+++ frontsite/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import patterns, url, include
 from frontsite import views
+from django.contrib.auth.views import logout
 
 urlpatterns = patterns('',
-                       url(r'^$', views.index, name='index')
+   url(r'^$', views.IndexView.as_view(), name='index'),
+   url(r'^login$', views.login, name='login'),
+   url(r'^registration$', views.registration, name='registration'),
+   url(r'^logout$', logout, name='logout'),
 )
--- frontsite/views.py
+++ frontsite/views.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.views.generic import View
 
-def index(request):
-    return render(request, 'frontsite/index.html')
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'frontsite/index.html')
+
+# def index(request):
+#     return render(request, 'frontsite/index.html')
+
+def registration(request):
+    pass
+
+def login(request):
+    pass
