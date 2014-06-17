99e1d449ab24ddceaa5c31a91c7d0d7995c51e32
ssstrz <codecats@gmail.com>
2014-06-17 10:59:55 +0200

    wsgi works on debug True

--- kitty/urls.py
+++ kitty/urls.py
@@ -1,9 +1,18 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from django.conf import settings
+from django.contrib.staticfiles import views
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('frontsite.urls', namespace='frontsite')),
+    url(r'^admin/', include(admin.site.urls)),
 )
+
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^static/(?P<path>.*)$', views.serve),
+    ]
