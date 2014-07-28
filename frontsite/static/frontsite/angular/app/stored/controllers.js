d47d6ffeb979b83d8d7227bfbd4fb95075c3aa1f
ssstrz <codecats@gmail.com>
2014-07-28 19:30:09 +0200

    store ctrl

---
+++ frontsite/static/frontsite/angular/app/stored/controllers.js
@@ -0,0 +1,8 @@
+'use strict';
+  
+ var kittyStoredControllers = angular.module('kittyStoredControllers', []);
+  /* Controllers */
+ 
+ kittyApp.controller('ExportCtrl', ['$scope', function($scope) {
+	 console.log('aaa');
+ }]);
